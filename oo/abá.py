d8a21b2f7a278bafb6a23dfb126754493e8edf24
Gabi <gabiidifolco@gmail.com>
2020-07-22 18:58:14 -0300

    Criei o atributo complexo filhos.

--- oo/abá.py
+++ oo/abá.py
@@ -1,17 +1,22 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = 13):
+    def __init__(self, *filhos, nome = None, idade = 32):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return 'Nde .ara t.i porang'
+        return f'Nde .ara t.i porang {id(self)}'
+
 
 if __name__ == '__main__':
-    p = Pessoa('Guilherme,')
-    print(Pessoa.cumprimentar(p))
-    print(p.nome)
-    p.nome = 'Gabriely e'
-    print(p.nome)
-    p.nome = 'Gustavo!'
-    print(p.nome)
-    print(p.idade)
+    Gabriely = Pessoa(nome = 'Gabriely')
+    Gustavo = Pessoa(nome='Gustavo')
+    Guilherme = Pessoa(nome='Guilherme')
+    Livio = Pessoa(Gabriely, Gustavo, Guilherme, nome = 'Livio')
+    print(Pessoa.cumprimentar(Livio))
+    print(id(Livio))
+    print(Livio.cumprimentar())
+    print(Livio.nome)
+    print(Livio.idade)
+    for filho in Livio.filhos:
+        print(filho.nome)
